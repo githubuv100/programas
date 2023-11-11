17d4e5771ad1adf9afe9b0439871d727f9114b5d
githubuv100 <148834189+githubuv100@users.noreply.github.com>
2023-11-10 19:32:51 -0600

    Add files via upload

---
+++ Memoria 2 .cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int edades[] = {18, 20, 24, 28, 30, 32};
+    int *ptr;
+
+    ptr = edades;
+
+    cout << "Trabajo con enteros:" << endl << endl;
+
+    cout << "La dirección del primer elemento es: " << edades << endl;
+    cout << "El valor del primer elemento es: " << *edades << endl << endl;
+
+    cout << "La dirección del segundo elemento es: " << edades + 1 << endl;
+    cout << "El valor del segundo elemento es: " << *(edades + 1) << endl << endl;
+
+    cout << "La dirección del tercer elemento es: " << edades + 2 << endl;
+    cout << "El valor del tercer elemento es: " << *(edades + 2) << endl << endl;
+
+    cout << "La dirección del cuarto elemento es: " << edades + 3 << endl;
+    cout << "El valor del cuarto elemento es: " << *(edades + 3) << endl << endl;
+
+    cout << "La dirección del quinto elemento es: " << edades + 4 << endl;
+    cout << "El valor del quinto elemento es: " << *(edades + 4) << endl << endl;
+
+    cout << "La dirección del sexto elemento es: " << edades + 5 << endl;
+    cout << "El valor del sexto elemento es: " << *(edades + 5) << endl << endl;
+
+    cout << "Trabajo con cadenas:" << endl << endl;
+
+    char cadena[] = "ciber seguridad";
+    char *ptrc = cadena;
+
+    cout << "La dirección del primer elemento es: " << static_cast<void*>(&ptrc) << endl;
+    cout << "El valor del primer elemento es: " << *ptrc << endl << endl;
+
+    cout << "La dirección del segundo elemento es: " << static_cast<void*>(ptrc + 1) << endl;
+    cout << "El valor del segundo elemento es: " << *(ptrc + 1) << endl << endl;
+
+    cout << "La dirección del tercer elemento es: " << static_cast<void*>(ptrc + 2) << endl;
+    cout << "El valor del tercer elemento es: " << *(ptrc + 2) << endl << endl;
+
+    cout << "La dirección del cuarto elemento es: " << static_cast<void*>(ptrc + 3) << endl;
+    cout << "El valor del cuarto elemento es: " << *(ptrc + 3) << endl << endl;
+
+    return 0;
+}
+
